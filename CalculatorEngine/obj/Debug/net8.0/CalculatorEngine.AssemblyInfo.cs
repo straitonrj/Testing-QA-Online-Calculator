a18bb6c55deebcfeadc3d222b23709a106d70f43
RJ Straiton <94383945+straitonrj@users.noreply.github.com>
2024-04-22 16:38:50 -0400

    Blazor issue, buttons dont work, unsure of how to take in args and access user inputs

--- CalculatorEngine/obj/Debug/net8.0/CalculatorEngine.AssemblyInfo.cs
+++ CalculatorEngine/obj/Debug/net8.0/CalculatorEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1c96dffb74cb1b2fd1406b5e4d5ef21f9b90b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
